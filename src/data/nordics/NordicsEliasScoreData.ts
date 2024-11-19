a54431ce563348008a8c075e4038c95301e0cc5b
pg-los <65456911+pg-los@users.noreply.github.com>
2024-11-19 20:02:00 +0000

    Update NordicsEliasScoreData.ts

--- src/data/nordics/NordicsEliasScoreData.ts
+++ src/data/nordics/NordicsEliasScoreData.ts
@@ -25,7 +25,7 @@ namespace Private {
                 icon: TimeData.getQuestions(Region.Nordics, NordicCharacters.Elias)[0].icon,
                 outcomeDesc: "sugar",
                 outcomeType: OutcomeType.Health,
-                link: "https://www.greenbrownblue.com/food-and-health-explained/sugar/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-health-explained/sugar/"               
             },
             {
                 "outcome": "Homemade porridge is high in fibre and protein and can be a healthier alternative to processed, sugary breakfast cereals. Finland produces 13% of the oats grown in the European Union.",
@@ -39,7 +39,7 @@ namespace Private {
                 icon: TimeData.getQuestions(Region.Nordics, NordicCharacters.Elias)[1].icon,
                 outcomeDesc: "whole grain",
                 outcomeType: OutcomeType.Health,
-                link: "https://www.greenbrownblue.com/food-and-health-explained/whole-grain-meaning/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-health-explained/whole-grain-meaning/"               
             },
             // Easter egg
             {
@@ -64,7 +64,7 @@ namespace Private {
                 icon: TimeData.getQuestions(Region.Nordics, NordicCharacters.Elias)[3].icon,
                 outcomeDesc: "whole grains",
                 outcomeType: OutcomeType.Health,
-                link: "https://www.greenbrownblue.com/food-and-health-explained/whole-grain-meaning/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-health-explained/whole-grain-meaning/"               
             }
         ],
         [NordicQuestions.Snacks1]: [
@@ -80,7 +80,7 @@ namespace Private {
                 icon: MCQData.getQuestions(Region.Nordics, NordicCharacters.Elias)[0][0].icon,
                 outcomeDesc: "Sodium",
                 outcomeType: OutcomeType.Health,
-                link: "https://www.greenbrownblue.com/food-and-health-explained/salt/"              
+                link: "https://greenbrownblue.kinsta.cloud/food-and-health-explained/salt/"              
             },
             {
                 "outcome": "Great choice! One apple provides at least 10 percent of the recommended daily amount of vitamin C and fiber. It also has a lot of dietary fiber that can act in several ways to lower cancer risk.",
@@ -94,7 +94,7 @@ namespace Private {
                 icon: MCQData.getQuestions(Region.Nordics, NordicCharacters.Elias)[0][1].icon,
                 outcomeDesc: "Nutrition related diseases",
                 outcomeType: OutcomeType.Healthcare,
-                link: "https://www.greenbrownblue.com/food-and-healthcare-explained/food-is-medicine/"              
+                link: "https://greenbrownblue.kinsta.cloud/food-and-healthcare-explained/food-is-medicine/"              
             },
             // easter egg
             {
@@ -119,7 +119,7 @@ namespace Private {
                 icon: MCQData.getQuestions(Region.Nordics, NordicCharacters.Elias)[0][3].icon,
                 outcomeDesc: "food insecurity",
                 outcomeType: OutcomeType.Culture,
-                link: "https://www.greenbrownblue.com/food-and-culture-explained/food-access-challenges/"                              
+                link: "https://greenbrownblue.kinsta.cloud/food-and-culture-explained/food-access-challenges/"                              
             }
         ],
         [NordicQuestions.Lunch1]: [
@@ -135,7 +135,7 @@ namespace Private {
                 icon: LunchData.getQuestions(Region.Nordics, NordicCharacters.Elias)[0][0].icon,
                 outcomeDesc: "Rice and methane",
                 outcomeType: OutcomeType.Climate,
-                link: "https://www.greenbrownblue.com/food-and-climate-explained/rice-climate-footprint/"
+                link: "https://greenbrownblue.kinsta.cloud/food-and-climate-explained/rice-climate-footprint/"
             },
             {
                 "outcome": "Subsituting the meat in bolognese with vegetables can be a great way to reduce your consumption of meat. Try adding mushrooms for texture and flavor.",
@@ -149,7 +149,7 @@ namespace Private {
                 icon: LunchData.getQuestions(Region.Nordics, NordicCharacters.Elias)[0][1].icon,
                 outcomeDesc: "protein",
                 outcomeType: OutcomeType.Health,
-                link: "https://www.greenbrownblue.com/food-and-health-explained/plant-based-proteins/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-health-explained/plant-based-proteins/"               
             },
             {
                 "outcome": "Fatty fish is important source of healthy fats. However, overfishing means that 50% of the world's fisheries are no longer sustainable. Be sure to choose sustainably caught or farmed fish. Organizations such as the World Wildlife Foundation and the Marine Stewardship Council offer great guides to help you choose which fish you should eat.",
@@ -163,7 +163,7 @@ namespace Private {
                 icon: LunchData.getQuestions(Region.Nordics, NordicCharacters.Elias)[0][2].icon,
                 outcomeDesc: "Sustainable fishing",
                 outcomeType: OutcomeType.Environment,
-                link: "https://www.greenbrownblue.com/food-and-environment-explained/seafood-climate-change/"              
+                link: "https://greenbrownblue.kinsta.cloud/food-and-environment-explained/seafood-climate-change/"              
             },
             {
                 "outcome": "Karelian Pies are a traditional dish from Finland. These are commonly served for lunch. Did you know that Finland is the first country in the world to serve a free, healthy school lunch to every student?",
@@ -177,7 +177,7 @@ namespace Private {
                 icon: LunchData.getQuestions(Region.Nordics, NordicCharacters.Elias)[0][3].icon,
                 outcomeDesc: "Traditional ",
                 outcomeType: OutcomeType.Culture,
-                link: "https://www.greenbrownblue.com/food-and-culture-explained/traditional-nordic-cuisine/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-culture-explained/traditional-nordic-cuisine/"               
             }
         ],
         [NordicQuestions.Lunch1Drinks]: [
@@ -248,7 +248,7 @@ namespace Private {
                 icon: YesNoSocialData.getQuestions(Region.Nordics, NordicCharacters.Elias)[0].icon,
                 outcomeDesc: "Fast Food",
                 outcomeType: OutcomeType.Healthcare,
-                link: "https://www.greenbrownblue.com/food-and-healthcare-explained/obesity-fast-food/"              
+                link: "https://greenbrownblue.kinsta.cloud/food-and-healthcare-explained/obesity-fast-food/"              
             }
         ],
         [NordicQuestions.Dinner2]: [
@@ -264,7 +264,7 @@ namespace Private {
                 icon: YesNoSocialData.getQuestions(Region.Nordics, NordicCharacters.Elias)[1].icon,
                 outcomeDesc: "Pesticides",
                 outcomeType: OutcomeType.Environment,
-                link: "https://www.greenbrownblue.com/food-and-environment-explained/organic-foods/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-environment-explained/organic-foods/"               
             }
         ],
         [NordicQuestions.Dinner3]: [
@@ -280,7 +280,7 @@ namespace Private {
                 icon: YesNoSocialData.getQuestions(Region.Nordics, NordicCharacters.Elias)[2].icon,
                 outcomeDesc: "fruits and vegetables",
                 outcomeType: OutcomeType.Health,
-                link: "https://www.greenbrownblue.com/food-and-health-explained/vegetables-fruits/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-health-explained/vegetables-fruits/"               
             }
         ],
         [NordicQuestions.Dinner4]: [
@@ -296,7 +296,7 @@ namespace Private {
                 icon: YesNoSocialData.getQuestions(Region.Nordics, NordicCharacters.Elias)[3].icon,
                 outcomeDesc: "processed foods",
                 outcomeType: OutcomeType.Health,
-                link: "https://www.greenbrownblue.com/food-and-health-explained/minimally-processed-vs-processed/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-health-explained/minimally-processed-vs-processed/"               
             }
         ],
         [NordicQuestions.DinnerProtein]: [
@@ -312,7 +312,7 @@ namespace Private {
                 icon: NordicsEliasPlateData.getItems()[0][0].foodInfo.content,
                 outcomeDesc: "Beef",
                 outcomeType: OutcomeType.Climate,
-                link: "https://www.greenbrownblue.com/food-and-climate-explained/beef-climate-change/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-climate-explained/beef-climate-change/"               
             },
             {
                 "outcome": "Products derived from animals, like cheese, require significantly more land to produce than protein-rich plant-based foods like fava beans.",
@@ -326,7 +326,7 @@ namespace Private {
                 icon: NordicsEliasPlateData.getItems()[0][1].foodInfo.content,
                 outcomeDesc: "Dairy",
                 outcomeType: OutcomeType.Climate,
-                link: "https://www.greenbrownblue.com/food-and-climate-explained/dairy-climate/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-climate-explained/dairy-climate/"               
             },
             {
                 "outcome": "White fish, if sustainble sourced, is a good protein option.",
@@ -340,7 +340,7 @@ namespace Private {
                 icon: NordicsEliasPlateData.getItems()[0][2].foodInfo.content,
                 outcomeDesc: "Nordic food ",
                 outcomeType: OutcomeType.Culture,
-                link: "https://www.greenbrownblue.com/food-and-culture-explained/traditional-nordic-cuisine/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-culture-explained/traditional-nordic-cuisine/"               
             },
             {
                 "outcome": "Salmon, if sustainably sourced, is a healthy protein option that also provides a lot of good omega 3 fatty acids. If from aquaculture production however, be aware that sometimes the salmon may be fed on fishmeal (making it a very resource intense production) and sometimes the feed is soy which if not certified, is implicated in deforestation.",
@@ -354,7 +354,7 @@ namespace Private {
                 icon: NordicsEliasPlateData.getItems()[0][3].foodInfo.content,
                 outcomeDesc: "tofu",
                 outcomeType: OutcomeType.Environment,
-                link: "https://www.greenbrownblue.com/food-and-environment-explained/soybean-impact/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-environment-explained/soybean-impact/"               
             }
         ],
         [NordicQuestions.DinnerGrains]: [
@@ -370,7 +370,7 @@ namespace Private {
                 icon: NordicsEliasPlateData.getItems()[1][0].foodInfo.content,
                 outcomeDesc: "Food marketing",
                 outcomeType: OutcomeType.Culture,
-                link: "https://www.greenbrownblue.com/food-and-culture-explained/food-marketing/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-culture-explained/food-marketing/"               
             },
             {
                 "outcome": "Buckwheat is a healthy grain that has seen a revival in the Nordics in the past 10 years.",
@@ -384,7 +384,7 @@ namespace Private {
                 icon: NordicsEliasPlateData.getItems()[1][1].foodInfo.content,
                 outcomeDesc: "Whole grains",
                 outcomeType: OutcomeType.Health,
-                link: "https://www.greenbrownblue.com/food-and-health-explained/whole-grain-meaning/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-health-explained/whole-grain-meaning/"               
             },
             {
                 "outcome": "Potato is culturally an important staple in the Nordics, and an important part of many traditional dishes. As the food culture is globalized, the Nordic population is however eating less and less potato.",
@@ -398,7 +398,7 @@ namespace Private {
                 icon: NordicsEliasPlateData.getItems()[1][2].foodInfo.content,
                 outcomeDesc: "Food marketing",
                 outcomeType: OutcomeType.Culture,
-                link: "https://www.greenbrownblue.com/food-and-culture-explained/food-marketing/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-culture-explained/food-marketing/"               
             },
             {
                 "outcome": "Whole grain bread has a lot of dietary fibers which promotes digestive health and is a healthy choice if you want to have a side-snack in your meal.",
@@ -412,7 +412,7 @@ namespace Private {
                 icon: NordicsEliasPlateData.getItems()[1][3].foodInfo.content,
                 outcomeDesc: "Whole grains",
                 outcomeType: OutcomeType.Health,
-                link: "https://www.greenbrownblue.com/food-and-health-explained/whole-grain-meaning/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-health-explained/whole-grain-meaning/"               
             }
         ],
         [NordicQuestions.DinnerVeggies]: [
@@ -428,7 +428,7 @@ namespace Private {
                 icon: NordicsEliasPlateData.getItems()[2][0].foodInfo.content,
                 outcomeDesc: "Leafy greens",
                 outcomeType: OutcomeType.Health,
-                link: "https://www.greenbrownblue.com/food-and-health-explained/which-vegetables/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-health-explained/which-vegetables/"               
             },
             {
                 "outcome": "Spinach is a good complement to other types of salad greens as it often includes more calcium, vintamin A and C and iron. During the summer spinach grows well also in the Nordic region.",
@@ -442,7 +442,7 @@ namespace Private {
                 icon: NordicsEliasPlateData.getItems()[2][1].foodInfo.content,
                 outcomeDesc: "Local food",
                 outcomeType: OutcomeType.Culture,
-                link: "https://www.greenbrownblue.com/food-and-culture-explained/eating-locally/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-culture-explained/eating-locally/"               
             },
             {
                 "outcome": "Green peas are truly a great staple to have in the fridge. If you do, you can without much effort add some healthy vegetables to your meal. Because they are frozen, you might also reduce the risk of food waste since many other greens are more easily perishable when fresh.",
@@ -456,7 +456,7 @@ namespace Private {
                 icon: NordicsEliasPlateData.getItems()[2][2].foodInfo.content,
                 outcomeDesc: "Food waste",
                 outcomeType: OutcomeType.Environment,
-                link: "https://www.greenbrownblue.com/food-and-climate-explained/food-waste-climate/"               
+                link: "https://greenbrownblue.kinsta.cloud/food-and-climate-explained/food-waste-climate/"               
             },
             {
                 "outcome": "Did you know that Fennel is a flowering plant species in the carrot family? This is a veggie that helps add a lot of flavour (anis-like) to any dish. It is seen as an important food  in the Nordic diet.",
@@ -470,7 +470,7 @@ namespace Private {
                 icon: NordicsEliasPlateData.getItems()[2][3].foodInfo.content,
                 outcomeDesc: "Food diversity",
                 outcomeType: OutcomeType.Health,
-                link: "https://www.greenbrownblue.com/food-and-health-explained/diverse-diets/"              
+                link: "https://greenbrownblue.kinsta.cloud/food-and-health-explained/diverse-diets/"              
             }
         ]
     };
